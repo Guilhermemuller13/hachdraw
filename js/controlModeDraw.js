3fe272d21a58895c38dc38638cc6ee9b0f315573
Guilherme Müller <54514054+Guilhermemuller13@users.noreply.github.com>
2024-10-17 12:00:36 -0300

    update page title to reflect active drawing mode

--- js/controlModeDraw.js
+++ js/controlModeDraw.js
@@ -6,14 +6,17 @@ let currentMode = enumModes.view;
 
 const handleChangeModeDrawer = () => {
   const appRef = document.querySelector(".app");
+  const changeButtonRef = document.querySelector("#change-mode");
 
   if (currentMode === enumModes.view) {
     appRef.classList.add("mode-draw");
     currentMode = enumModes.draw;
     enalbleDrawCanvas();
+    changeButtonRef.textContent = "Desativar modo de desenho";
   } else {
     appRef.classList.remove("mode-draw");
     currentMode = enumModes.view;
     disableDrawCanvas();
+    changeButtonRef.textContent = "Ativar modo de desenho";
   }
 };
